db9b8cf7428eccdd12a1217ca894f4436a39f20f
dennis <118351475+technoshantoms@users.noreply.github.com>
2024-03-24 14:53:01 +0300

    ..

--- frontend/src/modules/AuthModule/SideContent.jsx
+++ frontend/src/modules/AuthModule/SideContent.jsx
@@ -1,16 +1,12 @@
 import { Space, Layout, Divider, Typography } from 'antd';
 import logo from '@/style/images/idurar-crm-erp.svg';
 import useLanguage from '@/locale/useLanguage';
-import { useSelector } from 'react-redux';
-import { selectLangDirection } from '@/redux/translate/selectors';
 
 const { Content } = Layout;
 const { Title, Text } = Typography;
 
 export default function SideContent() {
   const translate = useLanguage();
-  const langDirection = useSelector(selectLangDirection)
-
   return (
     <Content
       style={{
@@ -26,23 +22,23 @@ export default function SideContent() {
           src={logo}
           alt="IDURAR ERP CRM"
           style={{ margin: '0 auto 40px', display: 'block' }}
-          height={63}
-          width={220}
+          height={140}
+          width={330}
         />
-        <div className="space40"></div>
-        <Title level={3}>{translate('Manage your company with')} :</Title>
-
+         <div className="space40"></div>
+        <Title level={3}>Homepesa (CLOUD HOSTED) is open for use by companies, churches, & and SACCOs</Title>
         <div className="space20"></div>
-        <ul className="list-checked" style={{paddingRight:0}}>
-          <li className={`list-checked-item ${langDirection === "rtl" ? "list-checked-item-right" : "list-checked-item-left"}`}>
+        <ul className="list-checked">
+          <li className="list-checked-item">
             <Space direction="vertical">
               <Text strong>{translate('All-in-one tool')}</Text>
 
-              <Text>{translate('Run and scale your ERP CRM Apps')}</Text>
+              <Text>This multi-currency paymeny,erp, & crm software is mainatined by: www.saltiam.com  
+              </Text>
             </Space>
           </li>
 
-          <li className={`list-checked-item ${langDirection === "rtl" ? "list-checked-item-right" : "list-checked-item-left"}`}>
+          <li className="list-checked-item">
             <Space direction="vertical">
               <Text strong>{translate('Easily add and manage your services')}</Text>
               <Text>{translate('It brings together your invoice clients and leads')}</Text>
@@ -56,66 +52,6 @@ export default function SideContent() {
             justifyContent: 'space-between',
           }}
         >
-          {/* <img
-            src={logo1}
-            alt="Logo1"
-            style={{
-              margin: '0 15px',
-              display: 'block',
-              float: 'left',
-              width: '48px',
-              filter: 'grayscale(1)',
-              mixBlendMode: 'multiply',
-              opacity: '0.8',
-            }}
-            height={48}
-            width={48}
-          />
-          <img
-            src={logo2}
-            alt="Logo2"
-            style={{
-              margin: '0 15px',
-              display: 'block',
-              float: 'left',
-              width: '48px',
-              filter: 'grayscale(1)',
-              mixBlendMode: 'multiply',
-              opacity: '0.8',
-            }}
-            height={48}
-            width={48}
-          />
-          <img
-            src={logo3}
-            alt="Logo3"
-            style={{
-              margin: '0 15px',
-              display: 'block',
-              float: 'left',
-              width: '48px',
-              filter: 'grayscale(1)',
-              mixBlendMode: 'multiply',
-              opacity: '0.8',
-            }}
-            height={48}
-            width={48}
-          />
-          <img
-            src={logo4}
-            alt="Logo4"
-            style={{
-              margin: '0 15px',
-              display: 'block',
-              float: 'left',
-              width: '48px',
-              filter: 'grayscale(1)',
-              mixBlendMode: 'multiply',
-              opacity: '0.8',
-            }}
-            height={48}
-            width={48}
-          /> */}
         </div>
       </div>
     </Content>
